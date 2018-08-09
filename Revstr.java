7a2b07ceffce3e4df8296a098fe542b7627efcb9
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-09 12:41:59 +0530

    Update Revstr.java

--- Revstr.java
+++ Revstr.java
@@ -11,7 +11,7 @@ class Revstr
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		for(int i=str.length()-1;i>0;i--)
+		for(int i=str.length()-1;i>=0;i--)
 		{
 			System.out.print(str.charAt(i));
 		}
