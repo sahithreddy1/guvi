7b2ea994cf09f118d081640c053eb68e4daa3bc3
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-09 11:54:42 +0530

    Update Evenorodd.java

--- Evenorodd.java
+++ Evenorodd.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Evenorodd
 {
-  public static void main(string args[])
+  public static void main(String args[])
   {
   Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
@@ -9,10 +9,7 @@ class Evenorodd
     {
     System.out.println("Even");
     }
-    else if(a%2!=0)
-    {
-    System.out.println("Odd");
-    }
+   
     else
     {
     System.out.println("invalid");
