edf44e13d4b539536412557baa91921dac5d0bb4
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-09 11:48:56 +0530

    Create Evenorodd.java

---
+++ Evenorodd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Evenorodd
+{
+  public static void main(string args[])
+  {
+  Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt()
+    if(a%2==0)
+    {
+    System.out.println("Even");
+    }
+    else if(a%2!=0)
+    {
+    System.out.println("Odd");
+    }
+    else
+    {
+    System.out.println("invalid");
+    }
+    }
+    }
+    
+  
+  
