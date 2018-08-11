baa78148950fc66f95c78cfb6a58cbbebab2a0c5
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-11 12:38:46 +0530

    Update Vowelorconsonant.java

--- Vowelorconsonant.java
+++ Vowelorconsonant.java
@@ -12,12 +12,12 @@ class Vowelorconsonant
 		Scanner sc=new Scanner(System.in);
 		char ch=sc.next().charAt(0);
 		if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-		System.out.println( " vowel");
+		System.out.println( " Vowel");
 		}
 		else if(ch=='b' || ch=='c' || ch=='d' || ch=='f' || ch=='g' || ch=='h' || ch=='j' || ch=='k' || ch=='l' || ch=='m' || ch=='n' || ch=='p' || ch=='q' || ch=='r' || ch=='s' || ch=='t' || ch=='v' || ch=='w' || ch=='x' || ch=='y' || ch=='z'){
 			
 		
-		System.out.println( " consonant");
+		System.out.println( " Consonant");
 		}
 		else
 		{
