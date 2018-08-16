516120130962cd8c84d0e790a992e1b38c0757b6
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-16 11:08:40 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -2,24 +2,20 @@
 
 import java.util.*;
 import java.lang.*;
-import java.io.*;
-
+import java.io.*:
 /* Name of the class has to be "Main" only if the class is public. */
 class Leapyear
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-
         int year;
         Scanner s = new Scanner(System.in);
         year = s.nextInt();
         boolean leap = false;
-
         if(year % 4 == 0)
         {
             if( year % 100 == 0)
             {
-                
                 if ( year % 400 == 0)
                     leap = true;
                 else
@@ -29,8 +25,7 @@ class Leapyear
                 leap = true;
         }
         else
-            leap = false;
-
+            leap = false:
         if(leap)
             System.out.println("Yes");
         else
