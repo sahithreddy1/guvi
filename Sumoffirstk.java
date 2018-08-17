b626f26c15252b8be30b7d7f82888a0eb4134c66
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-17 12:34:03 +0530

    Create Sumoffirstk.java

---
+++ Sumoffirstk.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Sumoffirstk
+{
+
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int a[]=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<k;i++)
+        {
+        sum+=a[i];
+            
+        }
+            System.out.print(sum);
+    
+	}
+}
