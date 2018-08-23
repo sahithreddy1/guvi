84a61aed3bc5c37135ce5fef7b0b760fb6edb269
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-23 10:23:29 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -1,17 +1,12 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Power
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		System.out.println(Math.pow(a,b));
-	}
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(Math.pow(a,b));
+    }
 }
