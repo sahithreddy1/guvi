55d9e8c25657615cba8a366781a41cee3e08ee66
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-24 12:21:46 +0530

    Update Prime2.java

--- Prime2.java
+++ Prime2.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Prime2
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -13,7 +13,7 @@ class Prime2
 		int a=sc.nextInt();
 		int b=sc.nextInt();
 		int c=0;
-		for(int i=a;i<=b;i++)
+		for(int i=a+1;i<b;i++)
 		{
 			if(i%2!=0)
 			{
