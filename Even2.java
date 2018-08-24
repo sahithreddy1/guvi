aa3a63a329b4790ad3c56fcdab1a5da948479edd
sahithreddy1 <41812726+sahithreddy1@users.noreply.github.com>
2018-08-24 12:25:27 +0530

    Create Even2.java

---
+++ Even2.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Even2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=0;
+		for(int i=a+1;i<b;i++)
+		{
+			if(i%2==0)
+			{
+			System.out.print(i+" ");
+			
+		}
+			}
+}
+}
